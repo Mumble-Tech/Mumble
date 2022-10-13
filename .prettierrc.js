fd6fb5a85ce7f6217e755f5247948b56f3a7acb3
deondreE <deondreenglish45@gmail.com>
2022-10-12 23:32:41 -0600

    Added some configuration file, Prettier, Gitignore.

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+model.exports = {
+    pluginSearchDirs: ['.'],
+    useTabs: true,
+    printWidth: 100,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSameLine: false,
+    semi: true,
+    quoteProps: 'consistent',
+    plugins: ['prettier-plugin-go-template']
+}
