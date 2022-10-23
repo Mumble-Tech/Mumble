d5633e13046bf0a568d3ed1aa3907e63303a1f73
deondreE <deondreenglish45@gmail.com>
2022-10-23 14:06:22 -0500

    added desktop, using react for now, probably will switch to vue, maybe not

--- packages/interface/Index.ts
+++ packages/interface/Index.ts
@@ -1,9 +1,3 @@
 
 export * from './components/Heading';
 export * from './components/Button';
-
-// Landing
-export * from './components/landing/navContainer';
-export * from './components/landing/description';
-export * from './components/landing/footerContainer';
-export * from './components/landing/inputs';
