9e20872548a9c92a183ba61e848d9437298a8327
deondreE <deondreenglish45@gmail.com>
2022-11-09 19:20:23 -0600

    Add IOS-APP Template: DEV ENDS THERE

---
+++ apps/mobile/iosapp/iosapp/iosappApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosappApp.swift
+//  iosapp
+//
+//  Created by Deondre English on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct iosappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
