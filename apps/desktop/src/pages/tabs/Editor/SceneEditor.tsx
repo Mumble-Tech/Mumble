b2238b5de0e9e4536b9803eb8b575fb8120eeb7e
deondreE <deondreenglish45@gmail.com>
2022-11-14 18:32:48 -0600

    Basic file structure, added and formatted for new titlebar

---
+++ apps/desktop/src/pages/tabs/Editor/SceneEditor.tsx
@@ -0,0 +1,7 @@
+export const SceneEditor = () => {
+  return (
+    <div>
+      <h1>Scene Editor</h1>
+    </div>
+  );
+};
---
+++ apps/desktop/src/pages/tabs/Live/LiveView.tsx
@@ -0,0 +1,7 @@
+export const LiveView = () => {
+  return (
+    <div>
+      <h1>Live View</h1>
+    </div>
+  );
+};
