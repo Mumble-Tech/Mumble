acde45bbf59217e36edf65b38e4624b9cabba78a
deondreE <deondreenglish45@gmail.com>
2022-11-16 17:33:43 -0600

    Added tailwind to main prod

--- apps/desktop/src/components/Header/Navbar.tsx
+++ apps/desktop/src/components/Header/Navbar.tsx
@@ -3,8 +3,12 @@ import './navbar.scss';
 export const Navbar = () => {
   return (
     <div className="nav">
-      <span className="nav-item--active"><a>Editor</a></span>
-      <span className="nav-item"><a>Live</a></span>
+      <span className="nav-item--active">
+        <a>Editor</a>
+      </span>
+      <span className="nav-item">
+        <a>Live</a>
+      </span>
     </div>
   );
 };
