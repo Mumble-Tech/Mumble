89009e175d00c822026a30d280709f31f5068bcd
deondreE <deondreenglish45@gmail.com>
2022-11-29 20:05:54 -0600

    format

--- apps/desktop/.eslintrc.cjs
+++ apps/desktop/.eslintrc.cjs
@@ -1,27 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jest": true
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
     ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "react/react-in-jsx-scope": "off"
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
     },
-    "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"]
-}
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+    ],
+};
